68f294d10a6174e78a3ab2ac80f893da1ecd384e
Sean Langton <slangton1@gmail.com>
2018-10-11 02:25:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,7 +22,7 @@ def input_to_index(user_input)
 end
 
 def move(board, index, player_token)
-  board[index] = player_token)
+  board[index] = player_token
 end
 
 def position_taken?(board, location)
